4e06e366a62b926ce04f375222345b801f7f274b
csandovalm74 <147273843+csandovalm74@users.noreply.github.com>
2026-05-19 17:16:58 -0500

    SIstema Listo

---
+++ libros/urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('libros/', include('libros.urls')),
+]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.lista_libros, name='lista_libros'),
+    path('<int:libro_id>/', views.detalle_libro, name='detalle_libro'),
+    path('crear/', views.crear_libro, name='crear_libro'),
+    path('<int:libro_id>/editar/', views.editar_libro, name='editar_libro'),
+    path('<int:libro_id>/eliminar/', views.eliminar_libro, name='eliminar_libro'),
+]
